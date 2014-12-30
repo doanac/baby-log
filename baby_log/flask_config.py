09d38580d8f180d4417222536cee95c9fde616fe
Andy Doan <doanac@beadoan.com>
2014-12-30 15:51:53 -0600

    abs path for db

--- baby_log/flask_config.py
+++ baby_log/flask_config.py
@@ -1,8 +1,12 @@
 import os
 
+_here = os.path.abspath(os.path.dirname(__file__))
+
 DEBUG = False
 DBNAME = 'production.db'
 
 if os.environ.get('DEBUG') == '1':
     DEBUG = True
     DBNAME = 'tmp.db'
+
+DBNAME = os.path.join(_here, '../', DBNAME)
